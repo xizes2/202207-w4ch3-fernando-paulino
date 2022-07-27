80601634eac27d78642090d0838029cf5d6b5669
Fernando Paulino <xizes2@hotmail.com>
2022-07-27 23:17:35 +0200

    correct provider, change its name, add imports, correct index

--- src/container/Info/Info.tsx
+++ src/container/Info/Info.tsx
@@ -4,7 +4,7 @@ import { GlobalContext } from "../../store/context/GlobalContext";
 const Info = (): JSX.Element => {
   const { isCalling } = useContext(GlobalContext);
 
-  return <span className={isCalling ? "message" : "off"}>"Calling..."</span>;
+  return <span className={isCalling ? "message" : "off"}>Calling...</span>;
 };
 
 export default Info;
