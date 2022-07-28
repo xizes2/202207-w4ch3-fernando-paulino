beed5e083a7f6ec3768da49457e1ee3d815ad767
Fernando Paulino <xizes2@hotmail.com>
2022-07-28 23:11:12 +0200

    add conditional renderering to action buttons

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -1,16 +1,27 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../store/context/GlobalContext";
 import Display from "../Display/Display";
 import "./Actions.css";
 
 const Actions = (): JSX.Element => {
+  const { isCalling, phoneNumber } = useContext(GlobalContext);
+
   return (
     <div className="actions">
       <Display />
-      <a href="/" className="call">
-        Call
-      </a>
-      <a href="/" className="hang active">
-        Hang
-      </a>
+      {isCalling && (
+        <a
+          href="/"
+          className={`call${phoneNumber.length === 9 ? " active" : ""}`}
+        >
+          Call
+        </a>
+      )}
+      {!isCalling && (
+        <a href="/" className="hang active">
+          Hang
+        </a>
+      )}
     </div>
   );
 };
